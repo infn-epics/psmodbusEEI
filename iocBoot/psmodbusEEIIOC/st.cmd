f12574118b23375381de6e1a8f3ff4e936c06615
amichelo <andrea.michelotti@lnf.infn.it>
2025-12-17 11:15:30 +0100

    update unimag and registers

--- iocBoot/psmodbusEEIIOC/st.cmd
+++ iocBoot/psmodbusEEIIOC/st.cmd
@@ -9,7 +9,7 @@ psmodbusEEI_registerRecordDeviceDriver(pdbbase)
 # Configure IP port (adjust IP address and port as needed)
 # drvAsynIPPortConfigure("EEI_IP", "192.168.190.153:502", 0, 0, 0)
 
-drvAsynIPPortConfigure("EEI_IP", "192.168.190.155:502", 0, 0, 0)
+drvAsynIPPortConfigure("EEI_IP", "192.168.190.156:502", 0, 0, 0)
 
 # Enable Modbus interpose interface
 modbusInterposeConfig("EEI_IP", 0, 0, 0)
@@ -26,7 +26,8 @@ drvModbusAsynConfigure("EEI_HOLDING_RD", "EEI_IP", 1, 3, 0, 45, 0, 1000, "EEI")
 drvModbusAsynConfigure("EEI_HOLDING_WR", "EEI_IP",1, 16, 0, 45, 0, 0, "EEI")
 
 ## Load record instances
-dbLoadRecords("../../db/eei_ps.template","P=EEI:PS01,PORT=EEI_HOLDING_RD,PORT_WR=EEI_HOLDING_WR")
+dbLoadRecords("../../db/eei_ps.template","P=BTF:MAG:EEI:QUATB201,PORT=EEI_HOLDING_RD,PORT_WR=EEI_HOLDING_WR")
+dbLoadRecords("../../db/eei_ps_unimag.template","P=BTF:MAG:EEI:QUATB201,PORT=EEI_HOLDING_RD,PORT_WR=EEI_HOLDING_WR")
 
 ## Start IOC
 iocInit()
